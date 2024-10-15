429198b71749a8ad8b377909bccf0af3d457f1e1
naveenjangid027 <147740082+naveenjangid027@users.noreply.github.com>
2024-10-15 18:02:41 +0530

    Update ifelsecondition.py

--- ifelsecondition.py
+++ ifelsecondition.py
@@ -1,75 +1,75 @@
 # I have write the programme to using traffic signals condition stop , go , look ?
-#if (light == "red"):
- #   print("stop")
-#elif(light == "green"):
-    #print("go")
-#elif(light == "yellow"):
- #   print("look")
-#else:
-#    print("light is the broken")
-#    print("end of code when conditions is using ")
+if (light == "red"):
+    print("stop")
+elif(light == "green"):
+    print("go")
+elif(light == "yellow"):
+    print("look")
+else:
+    print("light is the broken")
+    print("end of code when conditions is using ")
 
 # CONTION TO CHECK THE NUMBER IS THE GREATER AND LESS THAN THE CODE 
-#num =5
-##
-#f(num >3):
- # print("greater than 3")
+num =5
+
+f(num >3):
+  print("greater than 3")
     
     
 #Write the programme to grade of the student in the  programming Grademarks >=90 grade =A , 90>marks>=80 grade = B, 80> marks >=70 grade ="C" , 70>marks , grade = D
-#marks = 90
-##   print("grade A")
-#elif( marks > 90 and marks >=80):
-#    print("grade B")
-#elif( marks<80 and marks>=70):
-#    print("grade C")
-#elif(marks >70 ):
-#    print("D")
-#else:
-#   print("the person are failed")
+marks = 90
+   print("grade A")
+elif( marks > 90 and marks >=80):
+  print("grade B")
+elif( marks<80 and marks>=70):
+    print("grade C")
+elif(marks >70 ):
+    print("D")
+else:
+   print("the person are failed")
 
 # Write a programme to the nested condition in the programme , in this programme  true condition in the nested programme the  this queestion ?
-#age = 14
-#if (age>=18):
- #   if (age >19):
- #       print("drive")
-  #  else:
-  #      print("not drive ")
-  #  print("you can drive ")
-#else:
-#    print("you can not be drive ")
+age = 14
+if (age>=18):
+    if (age >19):
+       print("drive")
+    else:
+        print("not drive ")
+    print("you can drive ")
+else:
+    print("you can not be drive ")
 
 # WAP to check if number enter by user is odd or even 
-#num = 5
-#if (num%2==0):
-  #  print("the number is even ")
-#else:
-#    print("the number is odd ")
+num = 5
+if (num%2==0):
+    print("the number is even ")
+else:
+    print("the number is odd ")
 
 #WAP to find the greatest of 3 number by the user 
-#a = int(input("enter the first number:"))
-#b = int(input("enter the second number:")) 
-#c = int(input("enter the third number:"))
+a = int(input("enter the first number:"))
+b = int(input("enter the second number:")) 
+c = int(input("enter the third number:"))
 
-#if(a>=b and a>=c):
-#    print("the first number is largest ")  
-#elif(b>=c and b>=a):
-#    print("the second number largest")
-#elif(c>=a and c>=b):
-#    print("the third number is largest")
-#else:
- #   print("the number is wrong")
+if(a>=b and a>=c):
+    print("the first number is largest ")  
+elif(b>=c and b>=a):
+    print("the second number largest")
+elif(c>=a and c>=b):
+    print("the third number is largest")
+else:
+    print("the number is wrong")
  
- # WAP to check if number ia a multiple of 7 or not
-#x = int(input("Enter number:"))
-#if(x%7 == 0):
- #   print("It is multiple ")
-#else:
-  #  print("It is not multiple ")
-    #
-#str = "chandra" +"prakash"+"jangid"
-#print(str)
-#len(str)
+  WAP to check if number ia a multiple of 7 or not
+x = int(input("Enter number:"))
+if(x%7 == 0):
+    print("It is multiple ")
+else:
+    print("It is not multiple ")
+    
+str = "chandra" +"prakash"+"jangid"
+print(str)
+len(str)
 
 # list = list is the data type that can be stored the data in the sequencial way 
 list = [3,54,534,334]
